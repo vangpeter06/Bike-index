74203cb982c0edbafb85fe748c84a119aab341ac
Peter Vang <vang_peter06@yahoo.com>
2022-06-30 13:36:33 -0700

    add inputs to bikeElements to get data from API.
    Co-authored-by: PJ Perry <pjperry28@gmail.com>

--- src/bikeElement.js
+++ src/bikeElement.js
@@ -1,35 +1,67 @@
-
-export default class BikeService {  
-  static async stolenBike() {
-    try {
-      const response = await fetch('https://bikeindex.org:443/api/v3/search');
-      const data = await response.json();
-      let new_array = [];
-      for(i = 0; i <= data.length; i++) {
-        new_array.push(data);
-        console.log(new_array);
-      }
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      
-    } catch(error) {
-      return error.message;
-    }
-  }
+// export default class BikeElement {  
+   function stolenBike() {
+    fetch(`https://bikeindex.org:443/api/v3/search`)
+      .then(response =>{
+        if (!response.ok){
+          throw Error("ERROR");
+        }
+        return response.json();
+    //  console.log(response);
+  })
+  .then (data => {
+   console.log(data.bikes)
+   const html = data.bikes.map( bikes => {
+     return `
+     <div class="bike">
+      <p> title : ${bikes.title}</p>
+      <p><img src"${bikes.url}" alt"${bikes.title}"</p>
+      <p> location : ${bikes.stolen_location}</p>
+      <p> date stolen: ${bikes.date_stolen}</p>
+      <p>bike model: ${bikes.frame_model}</p>
+      </div>`
+   }).join('');
+   console.log(html)
+   document.querySelector("#showBikes").insertAdjacentHTML('afterbegin',html)
+  })
+  .catch( error => {
+   console.log(error)
+  });
 }
+stolenBike();
+// }
+
+
 
 // export default class BikeService {  
-//   static stolenBike(bikes) {
+//   static stolenBike() {
 //     return fetch(`https://bikeindex.org:443/api/v3/search`)
 //       .then(function(response) {
 //         if (!response.ok) {
 //           throw Error(response.statusText);
 //         }
-//         return response.json(bikes);
+//         const data = response.json()
+//         console.log(data)
+//         return response.json();
 //       })
 //       .catch(function(error) {
 //         return error;
 //       });
 //   }
 // }
+
+// export default class BikeService {  
+//   static async stolenBike() {
+//     try {
+//       const response = await fetch('https://bikeindex.org:443/api/v3/search');
+//       const data = await response.json();
+     
+//       if (!response.ok) {
+//         throw Error(response.statusText);
+//       }
+      
+//     } catch(error) {
+//       return error.message;
+//     }
+//   }
+// }
+
